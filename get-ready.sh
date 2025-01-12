bd9e6e2f96d8532af64edd1777a5abb8006b3436
HRUAS <28587019+HRUAS@users.noreply.github.com>
2025-01-12 15:39:11 +0530

    Update get-ready.sh

--- get-ready.sh
+++ get-ready.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 apk add python3
-
 apk add py3-pip
 
 pip3 install boto3
 pip3 install awscli --upgrade --user
 
-export PATH=$PATH:~/.local/bin
+# Add ~/.local/bin to PATH and persist it
+if ! grep -q "export PATH=\$PATH:~/.local/bin" ~/.profile; then
+    echo 'export PATH=$PATH:~/.local/bin' >> ~/.profile
+fi
+
+# Source the profile to make the change take effect immediately
+source ~/.profile
